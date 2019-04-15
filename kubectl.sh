95cf113447f22e5f94f258bbf59f68ff1c8f97ac
Chris Lindsay <christopher.m.lindsay@gmail.com>
2019-04-15 13:44:26 -0400

    Adding region requirement

--- kubectl.sh
+++ kubectl.sh
@@ -3,7 +3,14 @@
 if [ -z "$CLUSTER" ]; then
     echo "Please provide an Amazon EKS cluster name by populating the CLUSTER environment variable"
     echo "e.g."
-    echo "docker run -e CLUSTER=mycluster maddox/kubectl get pods"
+    echo "docker run -e CLUSTER=mycluster devops-chris/kubectl get pods"
+    exit 1
+fi
+
+if [ -z "$REGION" ]; then
+    echo "Please provide an Amazon EKS cluster name by populating the CLUSTER environment variable"
+    echo "e.g."
+    echo "docker run -e CLUSTER=mycluster -e REGION=us-east-1 devops-chris/kubectl get pods"
     exit 1
 fi
 
@@ -13,8 +20,8 @@ mkdir -p ~/.kube
 apiVersion: v1
 clusters:
 - cluster:
-    server: $(aws eks describe-cluster --name ${CLUSTER} | jq -r .cluster.endpoint) 
-    certificate-authority-data: $(aws eks describe-cluster --name ${CLUSTER} | jq -r .cluster.certificateAuthority.data) 
+    server: $(aws eks describe-cluster --name ${CLUSTER} --region ${REGION}| jq -r .cluster.endpoint) 
+    certificate-authority-data: $(aws eks describe-cluster --name ${CLUSTER} --region ${REGION} | jq -r .cluster.certificateAuthority.data) 
   name: kubernetes
 contexts:
 - context:
